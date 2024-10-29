265e003327189060cb9408f9e49e3953c972a9dd
Joshua Shim z5479929 <joshua.shim2004@gmail.com>
2024-10-29 11:40:35 +1100

    made file for white move_generation

---
+++ tests/white_move_generation.test.cpp
@@ -0,0 +1,27 @@
+#include <catch2/catch_test_macros.hpp>
+#include "../src/engine/Board.hpp"
+#include <constants.hpp>
+
+TEST_CASE("pawns") {
+
+}
+
+TEST_CASE("knights") {
+
+}
+
+TEST_CASE("bishops") {
+
+}
+
+TEST_CASE("rooks") {
+
+}
+
+TEST_CASE("kings") {
+
+}
+
+TEST_CASE("queens") {
+
+}
